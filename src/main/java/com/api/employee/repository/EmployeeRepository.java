64e8649c8f222032ded83f49a5e9de551701567d
hemanthjeyaraj <92981565+hemanthjeyaraj@users.noreply.github.com>
2025-10-28 15:00:54 +0530

    Hemanth1
    * commitcheck
    
    * commitcheck
    
    ---------
    
    Co-authored-by: hemanthjeyaraj17 <hemanthradtech@gmail.com>

--- src/main/java/com/api/employee/repository/EmployeeRepository.java
+++ src/main/java/com/api/employee/repository/EmployeeRepository.java
@@ -8,5 +8,10 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    boolean existsByEmail(String email);
+
+
+
 }
 
--- src/main/java/com/api/employee/service/EmployeeService.java
+++ src/main/java/com/api/employee/service/EmployeeService.java
@@ -1,6 +1,7 @@
 package com.api.employee.service;
 
 import com.api.employee.entity.Employee;
+import com.api.employee.exception.DuplicateEmailException;
 import com.api.employee.repository.EmployeeRepository;
 import org.springframework.stereotype.Service;
 
@@ -18,6 +19,11 @@ public class EmployeeService {
 
     // CREATE employee
     public Employee createEmployee(Employee employee) {
+
+        if (repository.existsByEmail(employee.getEmail())) {
+            throw new DuplicateEmailException("Email already exists: " + employee.getEmail());
+        }
+
         return repository.save(employee);
     }
 
@@ -28,6 +34,11 @@ public class EmployeeService {
 
     // READ one employee by ID
     public Employee getEmployeeById(Long id) {
+
+        if (!repository.existsById(id)) {
+            throw new DuplicateEmailException("Employee Not exists: " + id);
+        }
+
         return repository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Employee not found with ID: " + id));
     }
@@ -35,6 +46,13 @@ public class EmployeeService {
     // UPDATE employee
     public Employee updateEmployee(Long id, Employee updatedEmployee) {
         Employee existing = getEmployeeById(id);
+
+        if (!existing.getEmail().equals(updatedEmployee.getEmail()) &&
+                repository.existsByEmail(updatedEmployee.getEmail())) {
+            throw new DuplicateEmailException("Email already exists: " + updatedEmployee.getEmail());
+        }
+
+
         existing.setName(updatedEmployee.getName());
         existing.setEmail(updatedEmployee.getEmail());
         existing.setDepartment(updatedEmployee.getDepartment());
